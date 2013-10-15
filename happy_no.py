b4a0416680feda9eb2bdb26fe2fd13c6e9cf6fab
takirohit <rohitkhatana.khatana@gmail.com>
2013-10-16 05:19:33 +0530

    adding happy no

--- happy_no.py
+++ happy_no.py
@@ -1,3 +1,30 @@
+'''
+    language: python
+
+Description of problem
+A happy number is defined by the following process: 
+
+Given a number X, square each digit and sum the results. Repeat this procedure on the resulting sum until you either reach 1, or fall into a repeating cycle which does not include 1. When this process ends in 1, X is a happy number.
+
+The first line of the input will be an integer N (1 <= N <= 100).
+
+Each of the following N lines represents a single test case, containing an integer X (1 <= X <= 100000).
+
+For each test case, print 'Y' if X is a happy number, and 'N' if not. No blank line between test cases.
+
+
+Sample input         Sample output
+
+4
+1                       Y
+7                       Y
+635                     Y
+699                     N   
+                        
+'''
+
+
+
 import sys
 
 def run():
